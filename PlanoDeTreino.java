37713609396bf3c6245c9c43603a9667b8f877dc
Matheus Vieira Mendes Pacheco <131549317+MathLothric@users.noreply.github.com>
2024-06-12 14:50:47 -0300

    Update PlanoDeTreino.java

--- PlanoDeTreino.java
+++ PlanoDeTreino.java
@@ -1,8 +1,38 @@
-import java.util.List;
+import java.util.ArrayList;
 
 public class PlanoDeTreino {
-    private String nivelDeDificuldade;
-    private List<String> exercicios;
+    private String dificuldade;
+    private ArrayList<String> exercicios;
 
-    
+    PlanoDeTreino(String dificuldade){
+        this.dificuldade = dificuldade;
+        exercicios = new ArrayList<>();
+    }
+
+    public void setDificuldade(String dificuldade){
+        this.dificuldade = dificuldade;
+    }
+
+    public String getDificuldade(){
+        return dificuldade;
+    }
+
+    public void setExercicios(String exercicio){
+        exercicios.add(exercicio);
+    }
+
+    public void getExercicios(){
+        for(String exercicio : exercicios){
+            System.out.println(exercicio);
+        }
+    }
+
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("Nível de dificuldade: ").append(dificuldade).append(", Lista de exercícios:\n");
+        for (String exercicio : exercicios) {
+            sb.append("- ").append(exercicio).append("\n");
+        }
+        return sb.toString();
+    }
 }
