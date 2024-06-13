5791e2b18554802b8aaeff30e92c73e67b5bf651
Matheus Vieira Mendes Pacheco <131549317+MathLothric@users.noreply.github.com>
2024-06-12 22:33:29 -0300

    Update Aluno.java

--- Aluno.java
+++ Aluno.java
@@ -1,7 +1,29 @@
 public class Aluno extends Pessoa {
     
     private int numMatricula;
-    private String planoDeTreino;
+    private PlanoDeTreino planoDeTreino;
     
-    public Aluno()
+    public Aluno(String nome, int idade, String cpf, String telefone, int numMatricula, PlanoDeTreino planoDeTreino){
+        super(nome, idade, cpf, telefone);
+        this.numMatricula = numMatricula;
+        this.planoDeTreino = planoDeTreino;
+    }
+
+    public void setNumMatricula(int numMatricula){
+        this.numMatricula = numMatricula;
+    }
+
+    public void setPlanoDeTreino(PlanoDeTreino planoDeTreino){
+        this.planoDeTreino = planoDeTreino;
+    }
+
+    public int getMatricula(){
+        return numMatricula;
+    }
+
+    public PlanoDeTreino getPlanoDeTreino(){
+        planoDeTreino.toString();
+        return planoDeTreino;
+    }
+
 }
